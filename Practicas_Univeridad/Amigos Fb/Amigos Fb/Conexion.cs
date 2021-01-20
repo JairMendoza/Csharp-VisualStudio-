842ece1a0c69a311f91ad6dd6a88f3ad3d5d7abb
Jair Emanuelle Mendoza Reynoso <59372415+JairMendoza@users.noreply.github.com>
2021-01-20 17:44:00 -0600

    Add files via upload

---
+++ Practicas_Univeridad/Amigos Fb/Amigos Fb/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Amigos_Fb
+{
+    public class Conexion
+    {
+        public static MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; database=BD_Amigos; Uid=root; pwd=1234567890;");
+
+            conectar.Open();
+            return conectar;
+        }
+    }
+}
