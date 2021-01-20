25252f86c8bc4744db7dba3a6755bd224d0c1155
Jair Emanuelle Mendoza Reynoso <59372415+JairMendoza@users.noreply.github.com>
2021-01-20 17:55:36 -0600

    Add files via upload

---
+++ Practicas_Univeridad/prINTERES/prINTERES/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace prINTERES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.txtcapital = new System.Windows.Forms.TextBox();
+            this.txtrazon = new System.Windows.Forms.TextBox();
+            this.txttiempo = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txttotalinteres = new System.Windows.Forms.TextBox();
+            this.txtmontofinal = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnnuevo);
+            this.groupBox1.Controls.Add(this.btncalcular);
+            this.groupBox1.Controls.Add(this.txttiempo);
+            this.groupBox1.Controls.Add(this.txtrazon);
+            this.groupBox1.Controls.Add(this.txtcapital);
+            this.groupBox1.Controls.Add(this.label);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(20, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(768, 229);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Interes";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txttotalinteres);
+            this.groupBox2.Controls.Add(this.txtmontofinal);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(20, 253);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(768, 185);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Capital:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Razon:";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(40, 183);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(55, 17);
+            this.label.TabIndex = 2;
+            this.label.Text = "Tiempo";
+            // 
+            // txtcapital
+            // 
+            this.txtcapital.Location = new System.Drawing.Point(140, 48);
+            this.txtcapital.Name = "txtcapital";
+            this.txtcapital.Size = new System.Drawing.Size(222, 22);
+            this.txtcapital.TabIndex = 3;
+            // 
+            // txtrazon
+            // 
+            this.txtrazon.Location = new System.Drawing.Point(140, 112);
+            this.txtrazon.Name = "txtrazon";
+            this.txtrazon.Size = new System.Drawing.Size(222, 22);
+            this.txtrazon.TabIndex = 4;
+            // 
+            // txttiempo
+            // 
+            this.txttiempo.Location = new System.Drawing.Point(140, 178);
+            this.txttiempo.Name = "txttiempo";
+            this.txttiempo.Size = new System.Drawing.Size(222, 22);
+            this.txttiempo.TabIndex = 5;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(447, 66);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(154, 36);
+            this.btncalcular.TabIndex = 6;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(447, 134);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(154, 39);
+            this.btnnuevo.TabIndex = 7;
+            this.btnnuevo.Text = "Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            this.btnnuevo.Click += new System.EventHandler(this.btnnuevo_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Total Interes:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Monto Final:";
+            // 
+            // txttotalinteres
+            // 
+            this.txttotalinteres.Location = new System.Drawing.Point(140, 57);
+            this.txttotalinteres.Name = "txttotalinteres";
+            this.txttotalinteres.Size = new System.Drawing.Size(222, 22);
+            this.txttotalinteres.TabIndex = 8;
+            // 
+            // txtmontofinal
+            // 
+            this.txtmontofinal.Location = new System.Drawing.Point(140, 122);
+            this.txtmontofinal.Name = "txtmontofinal";
+            this.txtmontofinal.Size = new System.Drawing.Size(222, 22);
+            this.txtmontofinal.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = ".:.Estructuras secuenciales.:.";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txttiempo;
+        private System.Windows.Forms.TextBox txtrazon;
+        private System.Windows.Forms.TextBox txtcapital;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txttotalinteres;
+        private System.Windows.Forms.TextBox txtmontofinal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Practicas_Univeridad/prINTERES/prINTERES/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prINTERES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            //Declatracion de variables 
+            double capital, razon, tiempo,interes,monto;
+            //Entrada de datos
+            capital = double.Parse(txtcapital.Text);
+            razon = double.Parse(txtrazon.Text);
+            tiempo = double.Parse(txttiempo.Text);
+            //Proseso
+            monto = Math.Pow((1 + razon), tiempo)*capital;
+            interes = monto - capital;
+            //salida de la informacion
+            txtmontofinal.Text = Convert.ToString (monto);
+            txttotalinteres.Text = Convert.ToString(interes);
+
+        }
+
+        private void btnnuevo_Click(object sender, EventArgs e)
+        {
+            txttotalinteres.Clear();
+            txtcapital.Clear();
+            txtmontofinal.Clear();
+            txtrazon.Clear();
+            txttiempo.Clear();
+        }        
+    }
+}
