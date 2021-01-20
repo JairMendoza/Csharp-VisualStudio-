ef5bef2769a547027a6651d79e6be6c342ce2570
Jair Emanuelle Mendoza Reynoso <59372415+JairMendoza@users.noreply.github.com>
2021-01-20 17:53:56 -0600

    Add files via upload

---
+++ Practicas_Univeridad/PREstructuta iterativa DO WHILE/PREstructuta iterativa DO WHILE/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PREstructuta_iterativa_DO_WHILE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcalumnos = new System.Windows.Forms.TextBox();
+            this.txtpromedio = new System.Windows.Forms.TextBox();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnsalir);
+            this.groupBox1.Controls.Add(this.btnIngresar);
+            this.groupBox1.Controls.Add(this.btnnuevo);
+            this.groupBox1.Controls.Add(this.txtcalumnos);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 198);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese Datos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtpromedio);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(15, 221);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(503, 131);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Promedio";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese Cantidad De Alumnos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Promedio:";
+            // 
+            // txtcalumnos
+            // 
+            this.txtcalumnos.Location = new System.Drawing.Point(206, 49);
+            this.txtcalumnos.Name = "txtcalumnos";
+            this.txtcalumnos.Size = new System.Drawing.Size(186, 22);
+            this.txtcalumnos.TabIndex = 1;
+            // 
+            // txtpromedio
+            // 
+            this.txtpromedio.Location = new System.Drawing.Point(141, 65);
+            this.txtpromedio.Name = "txtpromedio";
+            this.txtpromedio.Size = new System.Drawing.Size(251, 22);
+            this.txtpromedio.TabIndex = 2;
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(52, 134);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnnuevo.TabIndex = 2;
+            this.btnnuevo.Text = "Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            this.btnnuevo.Click += new System.EventHandler(this.btnnuevo_Click);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(206, 134);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 3;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(359, 134);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 4;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 364);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.TextBox txtcalumnos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtpromedio;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Practicas_Univeridad/PREstructuta iterativa DO WHILE/PREstructuta iterativa DO WHILE/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PREstructuta_iterativa_DO_WHILE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables
+            int cantalumnos, nota;
+            double promedio = 0;
+            //Entrada de datos
+            cantalumnos = Convert.ToInt32(txtcalumnos.Text);
+            //Proceso
+            //Implementar un bucle for para pedir la nota de los alumnos
+            for(int i=1;i<=cantalumnos;i++)
+            {
+                //Leer la nota del alumno i
+                do
+                {
+                    nota = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Ingrese Nota del Alumno "+i,"Registro Notas"));
+
+                } while (nota < 0 || nota > 20);
+                promedio += nota;
+            }
+            //Salida de la informacion
+            txtpromedio.Text =Convert.ToString (promedio / cantalumnos);
+        }
+
+        private void btnnuevo_Click(object sender, EventArgs e)
+        {
+            txtpromedio.Clear();
+            txtcalumnos.Clear();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
