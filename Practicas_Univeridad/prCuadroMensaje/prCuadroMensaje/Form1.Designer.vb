b1b680a350f028de195539572b0ccd99a2e2873b
Jair Emanuelle Mendoza Reynoso <59372415+JairMendoza@users.noreply.github.com>
2021-01-20 17:52:03 -0600

    Add files via upload

---
+++ Practicas_Univeridad/prCuadroMensaje/prCuadroMensaje/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btningresar = New System.Windows.Forms.Button()
+        Me.btnsalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btningresar
+        '
+        Me.btningresar.Location = New System.Drawing.Point(42, 45)
+        Me.btningresar.Name = "btningresar"
+        Me.btningresar.Size = New System.Drawing.Size(238, 90)
+        Me.btningresar.TabIndex = 0
+        Me.btningresar.Text = "Ingrese Datos"
+        Me.btningresar.UseVisualStyleBackColor = True
+        '
+        'btnsalir
+        '
+        Me.btnsalir.Location = New System.Drawing.Point(304, 177)
+        Me.btnsalir.Name = "btnsalir"
+        Me.btnsalir.Size = New System.Drawing.Size(134, 60)
+        Me.btnsalir.TabIndex = 1
+        Me.btnsalir.Text = "Salir"
+        Me.btnsalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(489, 263)
+        Me.Controls.Add(Me.btnsalir)
+        Me.Controls.Add(Me.btningresar)
+        Me.Name = "Form1"
+        Me.Text = ".:.Cuadro de Mensaje.:."
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btningresar As Button
+    Friend WithEvents btnsalir As Button
+End Class
---
+++ Practicas_Univeridad/prCuadroMensaje/prCuadroMensaje/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub btningresar_Click(sender As Object, e As EventArgs) Handles btningresar.Click
+        'Declaracion de variables
+        Dim nombre As String
+        'Entrada de datos
+        nombre = InputBox("Ingrese Nombre", "Registro de Datos", "Nombre", 100, 0)
+        MessageBox.Show("Bienvenido Usuario: " & nombre, "Registro de Datos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+
+
+
+    End Sub
+
+    Private Sub btnsalir_Click(sender As Object, e As EventArgs) Handles btnsalir.Click
+        Dim opcion As DialogResult
+        opcion = MessageBox.Show("Realmente desea Salir de la App?", "Salir de la App", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If (opcion = Windows.Forms.DialogResult.Yes) Then
+            Me.Close()
+
+        End If
+    End Sub
+End Class
