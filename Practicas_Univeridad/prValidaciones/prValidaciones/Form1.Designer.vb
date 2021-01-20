81d22c8bbf57dff76ea7f973e5e722b4e0107928
Jair Emanuelle Mendoza Reynoso <59372415+JairMendoza@users.noreply.github.com>
2021-01-20 17:58:22 -0600

    Add files via upload

---
+++ Practicas_Univeridad/prValidaciones/prValidaciones/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblsalir = New System.Windows.Forms.LinkLabel()
+        Me.btnenviar = New System.Windows.Forms.Button()
+        Me.btnnuevo = New System.Windows.Forms.Button()
+        Me.txtdescripcion = New System.Windows.Forms.TextBox()
+        Me.txtsalario = New System.Windows.Forms.TextBox()
+        Me.txtedad = New System.Windows.Forms.TextBox()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.erroricono = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.ttmensaje = New System.Windows.Forms.ToolTip(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.erroricono, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblsalir)
+        Me.GroupBox1.Controls.Add(Me.btnenviar)
+        Me.GroupBox1.Controls.Add(Me.btnnuevo)
+        Me.GroupBox1.Controls.Add(Me.txtdescripcion)
+        Me.GroupBox1.Controls.Add(Me.txtsalario)
+        Me.GroupBox1.Controls.Add(Me.txtedad)
+        Me.GroupBox1.Controls.Add(Me.txtnombre)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(617, 316)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingrese Datos"
+        '
+        'lblsalir
+        '
+        Me.lblsalir.AutoSize = True
+        Me.lblsalir.Location = New System.Drawing.Point(512, 36)
+        Me.lblsalir.Name = "lblsalir"
+        Me.lblsalir.Size = New System.Drawing.Size(36, 17)
+        Me.lblsalir.TabIndex = 10
+        Me.lblsalir.TabStop = True
+        Me.lblsalir.Text = "Salir"
+        '
+        'btnenviar
+        '
+        Me.btnenviar.Location = New System.Drawing.Point(495, 213)
+        Me.btnenviar.Name = "btnenviar"
+        Me.btnenviar.Size = New System.Drawing.Size(92, 46)
+        Me.btnenviar.TabIndex = 9
+        Me.btnenviar.Text = "Enviar"
+        Me.btnenviar.UseVisualStyleBackColor = True
+        '
+        'btnnuevo
+        '
+        Me.btnnuevo.Location = New System.Drawing.Point(495, 139)
+        Me.btnnuevo.Name = "btnnuevo"
+        Me.btnnuevo.Size = New System.Drawing.Size(92, 43)
+        Me.btnnuevo.TabIndex = 8
+        Me.btnnuevo.Text = "Nuevo"
+        Me.btnnuevo.UseVisualStyleBackColor = True
+        '
+        'txtdescripcion
+        '
+        Me.txtdescripcion.Location = New System.Drawing.Point(107, 223)
+        Me.txtdescripcion.Multiline = True
+        Me.txtdescripcion.Name = "txtdescripcion"
+        Me.txtdescripcion.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtdescripcion.Size = New System.Drawing.Size(340, 87)
+        Me.txtdescripcion.TabIndex = 7
+        '
+        'txtsalario
+        '
+        Me.txtsalario.Location = New System.Drawing.Point(107, 160)
+        Me.txtsalario.Name = "txtsalario"
+        Me.txtsalario.Size = New System.Drawing.Size(100, 22)
+        Me.txtsalario.TabIndex = 6
+        '
+        'txtedad
+        '
+        Me.txtedad.Location = New System.Drawing.Point(107, 94)
+        Me.txtedad.Name = "txtedad"
+        Me.txtedad.Size = New System.Drawing.Size(100, 22)
+        Me.txtedad.TabIndex = 5
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(107, 38)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(340, 22)
+        Me.txtnombre.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(26, 226)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Descripcion:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 163)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Salario:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(26, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Edad:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombres:"
+        '
+        'erroricono
+        '
+        Me.erroricono.ContainerControl = Me
+        '
+        'ttmensaje
+        '
+        Me.ttmensaje.IsBalloon = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(637, 334)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = ".:.Validaciones.:."
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.erroricono, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblsalir As LinkLabel
+    Friend WithEvents btnenviar As Button
+    Friend WithEvents btnnuevo As Button
+    Friend WithEvents txtdescripcion As TextBox
+    Friend WithEvents txtsalario As TextBox
+    Friend WithEvents txtedad As TextBox
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents erroricono As ErrorProvider
+    Friend WithEvents ttmensaje As ToolTip
+End Class
---
+++ Practicas_Univeridad/prValidaciones/prValidaciones/Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Private Sub btnenviar_Click(sender As Object, e As EventArgs) Handles btnenviar.Click
+        Try
+            If Me.ValidateChildren And txtnombre.Text <> String.Empty And Val(txtedad.Text) - Int(Val(txtedad.Text) = 0 And txtsalario.Text <> String.Empty) Then
+                MessageBox.Show("Datos registrados correctamente", "Registro de Usuarios", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Ingrese correctamente los datos remarcados.", "Registro de usuarios", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub txtnombre_TextChanged(sender As Object, e As EventArgs) Handles txtnombre.TextChanged
+
+    End Sub
+
+    Private Sub txtnombre_Validating(sender As Object, e As CancelEventArgs) Handles txtnombre.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.erroricono.SetError(sender, "")
+        Else
+            Me.erroricono.SetError(sender, "Ingrese el nombre, este dato es obligatorio")
+        End If
+    End Sub
+
+    Private Sub txtsalario_TextChanged(sender As Object, e As EventArgs) Handles txtsalario.TextChanged
+
+    End Sub
+
+    Private Sub txtsalario_Validating(sender As Object, e As CancelEventArgs) Handles txtsalario.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.erroricono.SetError(sender, "")
+        Else
+            Me.erroricono.SetError(sender, "Ingrese el Salario, este dato es obligatorio")
+        End If
+    End Sub
+
+    Private Sub txtedad_TextChanged(sender As Object, e As EventArgs) Handles txtedad.TextChanged
+
+    End Sub
+
+    Private Sub txtedad_Validating(sender As Object, e As CancelEventArgs) Handles txtedad.Validating
+        If (Val(txtedad.Text) - Int(Val(txtedad.Text)) = 0) Then
+            Me.erroricono.SetError(sender, "")
+        Else
+            Me.erroricono.SetError(sender, "Ingrese una edad valida, este dato es obligatorio")
+        End If
+    End Sub
+
+    Private Sub btnnuevo_Click(sender As Object, e As EventArgs) Handles btnnuevo.Click
+        txtedad.Clear()
+        txtdescripcion.Clear()
+        txtnombre.Clear()
+        txtsalario.Clear()
+    End Sub
+
+    Private Sub lblsalir_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lblsalir.LinkClicked
+        End
+
+    End Sub
+
+    Private Sub txtnombre_MouseHover(sender As Object, e As EventArgs) Handles txtnombre.MouseHover
+        ttmensaje.SetToolTip(txtnombre, "Ingrese aqui el Nombre del Usuario.")
+        ttmensaje.ToolTipTitle = "Nombre del Usuario"
+        ttmensaje.ToolTipIcon = ToolTipIcon.Info
+
+
+    End Sub
+
+    Private Sub txtedad_MouseHover(sender As Object, e As EventArgs) Handles txtedad.MouseHover
+        ttmensaje.SetToolTip(txtedad, "Ingrese aqui la Edad del Usuario.")
+        ttmensaje.ToolTipTitle = "Edad del Usuario"
+        ttmensaje.ToolTipIcon = ToolTipIcon.Info
+    End Sub
+
+    Private Sub txtsalario_MouseHover(sender As Object, e As EventArgs) Handles txtsalario.MouseHover
+        ttmensaje.SetToolTip(txtsalario, "Ingrese aqui el Salario del Usuario.")
+        ttmensaje.ToolTipTitle = "Salario del Usuario"
+        ttmensaje.ToolTipIcon = ToolTipIcon.Info
+    End Sub
+End Class
